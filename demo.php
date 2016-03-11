240f4437d8ff8bd2ff8b9f336fdd133d5f95c58b
luoxun <luoxun007@gmail.com>
2016-03-11 15:41:33 +0800

    up

---
+++ demo.php
@@ -0,0 +1,10 @@
+<?php
+
+$chinese_text = "北京大学在四川招生";
+
+$_SEGMENT_BASE_URL = 'http://segment.sae.sina.com.cn/urlclient.php'
+
+//payload = urllib.urlencode([('context', chinese_text),])
+//args = urllib.urlencode([('word_tag', 1), ('encoding', 'UTF-8'),])
+//url = _SEGMENT_BASE_URL + '?' + args
+//result = urllib2.urlopen(url, payload).read();
