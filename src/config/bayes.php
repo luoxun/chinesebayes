2610e10a9ad101e1bef0e4bbf0a7bc76023381ef
luoxun <luoxun007@gmail.com>
2016-03-30 16:35:35 +0800

    up

--- src/config/bayes.php
+++ src/config/bayes.php
@@ -1,43 +1,16 @@
 <?php
 
-return array(
+/**
+ * Your package config would go here
+ */
 
-    /*
-     |--------------------------------------------------------------------------
-     | Debugbar Settings
-     |--------------------------------------------------------------------------
-     |
-     | Debugbar is enabled by default, when debug is set to true in app.php.
-     | You can override the value by setting enable to true or false instead of null.
-     |
-     */
-
-    'enabled' => null,
-
-    /*
-     |--------------------------------------------------------------------------
-     | Storage settings
-     |--------------------------------------------------------------------------
-     |
-     | DebugBar stores data for session/ajax requests.
-     | You can disable this, so the debugbar stores data in headers/session,
-     | but this can cause problems with large data collectors.
-     | By default, file storage (in the storage folder) is used. Redis and PDO
-     | can also be used. For PDO, run the package migrations first.
-     |
-     */
-    'storage' => array(
-        'enabled'    => true,
-        'driver'     => 'sqlite',
-    ),
+return [
+    
+    'driver' => 'sqlite',
 
     'sqlite' => array(
+        'file' => app_path('db'),
+     ),
 
-        'dbpath' => __DIR__,
-        'filename' => 'db'
-
-    ),
-    
-    'redis' => array(),
-
-);
+    'memery' => array(),
+];
